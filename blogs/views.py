19c5f7cee2923c06ed1363a89ee7315c4037a306
D6666666 <1657450321@qq.com>
2019-03-21 16:31:37 +0800

    优化模型排序、视图函数

--- blogs/views.py
+++ blogs/views.py
@@ -32,11 +32,11 @@ def detail(request, pk):
 def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
                                     created_time__month=month
-                                    ).order_by('-created_time')
+                                    )
     return render(request, 'index.html', context={'post_list': post_list})
 
 def category(request, pk):
     # 记得在开始部分导入 Category 类
     cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    post_list = Post.objects.filter(category=cate)
     return render(request, 'index.html', context={'post_list': post_list})
